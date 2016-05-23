38530a2ede4a6c6fb9f5442f67ebcbff9d2b7c95
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 16:26:02 -0300

    Configurações iniciais

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
-define('DB_USERNAME', 'adminiFj7Raf');
-define('DB_PASSWORD', 'hXHhRdDKJyUj');
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
 
 //$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-$dbh = new PDO("mysql:host=127.0.0.1;dbname=dominion", DB_USERNAME, DB_PASSWORD);
+//$dbh = new PDO("mysql:host="+127.0.0.1;dbname=dominion", DB_USERNAME, DB_PASSWORD);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
