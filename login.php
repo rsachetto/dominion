9629894541ebf0e71471634b61e5de8b54ed8fe8
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 17:12:18 -0300

    Configurações iniciais

--- login.php
+++ login.php
@@ -37,12 +37,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['user_id'] = $user_id;
             $_SESSION['role'] = $role;
 
-            echo($role);
+            $message= $role;
             
-            if($role == "organizador")
-                header("location:dashboard_organizer.php");
-            elseif ($role == "jogador")
-                header("location:dashboard_player.php");
+            //if($role == "organizador")
+            header("location:dashboard_organizer.php");
+            //elseif ($role == "jogador")
+            //    header("location:dashboard_player.php");
         }
 
 
