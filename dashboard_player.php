c78a38ac181821b1de3bd2ebb26cf49b0260954d
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 17:52:57 -0300

    Configurações iniciais

--- dashboard_player.php
+++ dashboard_player.php
@@ -5,7 +5,10 @@ include('session.php');
 
 <head>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 
     <title>Bem vindo jogador </title>
 </head>
@@ -14,8 +17,8 @@ include('session.php');
 <?php
 include('player_sidebar.php');
 ?>
-<h1>Welcome <?php echo $_SESSION['username']; ?></h1>
-<h2><a href = "logout.php">Sign Out</a></h2>
+<!--<h1>Welcome --><?php //echo $_SESSION['username']; ?><!--</h1>-->
+<!--<h2><a href = "logout.php">Sign Out</a></h2>-->
 </body>
 
 </html>
