47b254b6e49892db2384298e739ce100b0139ee1
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 18:21:42 -0300

    Configurações iniciais

--- organizer_sidebar.php
+++ organizer_sidebar.php
@@ -18,18 +18,6 @@
                         <i class="glyphicon glyphicon-home"></i>
                         Overview </a>
                 </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuarios <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a>
-                    <ul class="dropdown-menu forAnimate" role="menu">
-                        <li><a href="#">Crear</a></li>
-                        <li><a href="#">Modificar</a></li>
-                        <li><a href="#">Reportar</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Informes</a></li>
-                    </ul>
-                </li>
                 <li>
                     <a href="#">
                         <i class="glyphicon glyphicon-user"></i>
