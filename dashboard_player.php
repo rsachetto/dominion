ce1c47292424cf91f4cf3a188a55b5449a6155b7
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 17:59:36 -0300

    Configurações iniciais

--- dashboard_player.php
+++ dashboard_player.php
@@ -14,14 +14,20 @@ include('session.php');
 </head>
 
 <body>
-<?php
-include('player_sidebar.php');
-?>
-<div class="col-md-9">
-    <div class="profile-content">
-        Some user related content goes here...
+
+<div class="container">
+    <div class="row profile">
+        <?php
+        include('player_sidebar.php');
+        ?>
+        <div class="col-md-9">
+            <div class="profile-content">
+                Some user related content goes here...
+            </div>
+        </div>
     </div>
 </div>
+
 <!--<h1>Welcome --><?php //echo $_SESSION['username']; ?><!--</h1>-->
 <!--<h2><a href = "logout.php">Sign Out</a></h2>-->
 </body>
