54140f7b5dc8b4f6ee18d57be754bfaf6daee5d0
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 17:19:43 -0300

    Configurações iniciais

--- login.php
+++ login.php
@@ -37,7 +37,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['user_id'] = $user_id;
             $_SESSION['role'] = $role;
 
-            $message = 'Permissão: '.$role;
+            $message = 'Permissao: '.$role. 'Id: '.$user_id;
             
             //if($role == "organizador")
             //header("location:dashboard_organizer.php");
