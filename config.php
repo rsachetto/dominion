ec6c9ab0f5290aaf8935e5c90e1309ff346bd451
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 16:17:15 -0300

    Configurações iniciais

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
-define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'adminiFj7Raf');
 define('DB_PASSWORD', 'hXHhRdDKJyUj');
-define('DB_DATABASE', 'dominion');
+
 //$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-$dbh = new PDO("mysql:host=DB_SERVER;dbname=DB_DATABASE", DB_USERNAME, DB_PASSWORD);
+$dbh = new PDO("mysql:host=localhost;dbname=dominion", DB_USERNAME, DB_PASSWORD);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
