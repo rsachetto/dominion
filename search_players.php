718ebe341c9a54ac7ddf3964e68dd3e38cc652d2
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 20:50:49 -0300

    Configurações iniciais

--- search_players.php
+++ search_players.php
@@ -3,9 +3,9 @@ include("config.php");
 $query = $_GET['s'].'%'; // add % for LIKE query later
 
 // do query
-$stmt = $dbh->prepare('SELECT username FROM user WHERE username LIKE = ?');
-//$stmt->bindParam(':query', $query, PDO::PARAM_STR);
-$stmt->execute(array($query));
+$stmt = $dbh->prepare('SELECT username FROM user WHERE username LIKE :query');
+$stmt->bindParam(':query', $query, PDO::PARAM_STR);
+$stmt->execute();
 
 // populate results
 $results = array();
