feceec6f07d1d40e437bc71f50ba72a029380552
Rafael Sachetto <rsachetto@gmail.com>
2016-05-23 20:10:09 -0300

    Configurações iniciais

---
+++ search_users.php
@@ -0,0 +1,17 @@
+<?php
+include("config.php");
+$query = $_GET['query'].'%'; // add % for LIKE query later
+
+// do query
+$stmt = $dbh->prepare('SELECT username FROM user WHERE username LIKE = :query');
+$stmt->bindParam(':query', $query, PDO::PARAM_STR);
+$stmt->execute();
+
+// populate results
+$results = array();
+foreach ($stmt->fetchAll(PDO::FETCH_COLUMN) as $row) {
+    $results[] = $row;
+}
+
+// and return to typeahead
+return json_encode($results);
