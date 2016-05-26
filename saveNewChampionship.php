c5c43f85c1c5c3e28ab3bc28bf2b9686024d5cce
Rafael Sachetto <rsachetto@gmail.com>
2016-05-26 11:56:10 -0300

    Configurações iniciais

--- saveNewChampionship.php
+++ saveNewChampionship.php
@@ -2,10 +2,12 @@
 
 // Unescape the string values in the JSON array
 $players = stripcslashes($_POST['players']);
+error_log($players);
+
 
 // Decode the JSON array
 $players = json_decode($players,TRUE);
-
+error_log($players);
 
 // now $tableData can be accessed like a PHP array
-error_log($players[0]);
+
