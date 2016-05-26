cfbf1d32920f5bdabb01ed146c84458384c88290
Rafael Sachetto <rsachetto@gmail.com>
2016-05-26 12:00:36 -0300

    Configurações iniciais

--- saveNewChampionship.php
+++ saveNewChampionship.php
@@ -10,5 +10,8 @@ $players = json_decode($players,TRUE);
 if($players == NULL) {
     error_log("NULLLLL" + json_last_error_msg());
 }
+else {
+    error_log("NOT NULL");
+}
 // now $tableData can be accessed like a PHP array
 
