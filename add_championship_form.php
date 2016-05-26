7617e6fb1dbcbe8f6dbd550fac2cb3045cf3741e
Rafael Sachetto <rsachetto@gmail.com>
2016-05-26 10:46:31 -0300

    Configurações iniciais

--- add_championship_form.php
+++ add_championship_form.php
@@ -42,7 +42,13 @@
                     });
                 },
                 afterSelect: function (item) {
-                    $('#players-table > tbody:last-child').append('<tr><td>'+item.id+'</td><td>'+item.username+'</td><td>'+item.name+'</td></tr>');
+                    td1 = '<td>'+item.id+'</td>';
+                    td2 = '<td>'+item.username+'</td>';
+                    td3 = '<td>'+item.name+'</td>';
+
+                    tr = '<tr>'+td1+td2+td3+'</tr>';
+
+                    $('#players-table > tbody:last-child').append(tr);
                 },
 
                 displayText: function (item) {
@@ -50,8 +56,34 @@
                 }
 
             });
-        })
+
+            $('#submit').click(function() {
+                //Get raw HTML of tbody in the data table
+                var table = $('#players-table tbody').html();
+
+                for (var i = 0, row; row = table.rows[i]; i++) {
+                    //iterate through rows
+                    //rows would be accessed using the "row" variable assigned in the for loop
+                    for (var j = 0, col; col = row.cells[j]; j++) {
+                        //iterate through columns
+                        //columns would be accessed using the "col" variable assigned in the for loop
+                        console.log(col);
+                    }
+                }
+
+//                //build form HTML (hide keeps the form from being visible)
+//                $form = $('<form/>').attr({method: 'POST', action: ''}).hide();
+//                //build textarea HTML
+//                $textarea = $('<textarea/>').attr({name: 'data_rows'}).val(data_rows);
+//                //add textarea to form
+//                $form.append($textarea);
+//                //add form to the document body
+//                $('body').append($form);
+//                //submit the form
+//                $form.submit();
+            });
+        });
     </script>
 
-    <button type="submit" class="btn btn-default">Submit</button>
+    <button type="submit" class="btn btn-default">Salvar</button>
 </form>
